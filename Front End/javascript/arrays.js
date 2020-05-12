8c3d4aab52971442e35bf5df0d684b961168b00b
parikshit singh <pk223933@gmail.com>
2020-05-12 21:23:52 +0530

    arrays learnt

---
+++ Front End/javascript/arrays.js
@@ -0,0 +1,12 @@
+/* var arr=[1, 2, 3, 4];
+for(var i=0; i<4; i++)
+{
+    console.log(arr[i]);
+} */
+/* var arr= new Array(100);
+console.log(arr.length); */
+/* var a=[1, 2, 3, 44, 5]
+console.log(a.length); */
+/* var arr=new Array(1000000000);//max size is 10^9
+console.log(arr.length); */
+
