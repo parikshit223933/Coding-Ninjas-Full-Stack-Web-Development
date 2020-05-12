2ae64e8fcf470a3e9b69df7b212acefd8a51f4f8
parikshit singh <pk223933@gmail.com>
2020-05-13 00:55:22 +0530

    learnt

---
+++ Front End/javascript/objects.js
@@ -0,0 +1,10 @@
+/* var student={};
+console.log(typeof(student)); *///empty object
+
+var student = {
+    name: "abc",
+    roll_number: 43,
+    marks: 4
+};
+console.log(student.marks);
+//keys are always strings
