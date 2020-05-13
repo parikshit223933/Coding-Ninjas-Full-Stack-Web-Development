a956dfa163e39904f433fa876364a684b10488cf
parikshit singh <pk223933@gmail.com>
2020-05-13 11:05:09 +0530

    notations

--- Front End/javascript/objects.js
+++ Front End/javascript/objects.js
@@ -1,10 +1,18 @@
 /* var student={};
 console.log(typeof(student)); *///empty object
 
-var student = {
+/* var student = {
     name: "abc",
     roll_number: 43,
     marks: 4
 };
 console.log(student.marks);
 //keys are always strings
+ */
+
+ //ways of creating objects
+ var obj={};
+ //or
+ var obj=new Object();
+ obj={name:"parikshit", surname:"singh", age:21};
+ console.log(obj["name"], obj.surname);
