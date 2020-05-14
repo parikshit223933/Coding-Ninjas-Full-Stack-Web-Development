978999c91d988cb27be9540872fa1359c7f9c3bd
parikshit singh <pk223933@gmail.com>
2020-05-15 01:32:30 +0530

    learnt consts

---
+++ Front End/javascript/mini-projects using javascript/extras/const.js
@@ -0,0 +1,10 @@
+"use strict";
+var a=20;
+a=10;
+
+const c=20;
+c=30;//you cannot give a value to a constant variable later and
+//neither can you assign a value to a const variable after initialization.
+//you have to give them their value, then and there itself.
+//hoisting is done in case of consts but it is slightly different.
+//we connot refer to the const variables before their declaration and assignment.
