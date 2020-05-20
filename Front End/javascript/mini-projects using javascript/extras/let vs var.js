16c7afe8068364fa19cff049c586798ed6922bee
parikshit singh <pk223933@gmail.com>
2020-05-20 21:21:20 +0530

    learnt why to use "let"

---
+++ Front End/javascript/mini-projects using javascript/extras/let vs var.js
@@ -0,0 +1,13 @@
+/* for(var i=1; i<=5; i++)
+{
+    setTimeout(function(){
+        console.log(i);
+    }, 1000);
+} */
+
+for(let i=1; i<=5; i++)
+{
+    setTimeout(function(){
+        console.log(i);
+    }, 1000);
+}
