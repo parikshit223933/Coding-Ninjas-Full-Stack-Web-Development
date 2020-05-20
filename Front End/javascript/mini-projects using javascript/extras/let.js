284487b05465287d6bd41076cbff46316227e459
parikshit singh <pk223933@gmail.com>
2020-05-20 21:11:44 +0530

    let studied

---
+++ Front End/javascript/mini-projects using javascript/extras/let.js
@@ -0,0 +1,18 @@
+console.log(a);
+var a = 10;
+if (a <= 10) {
+    console.log(b);//error
+    let b=20;
+}
+
+/* var a=10;
+if(a<=10)
+{
+    let b=40;
+    //error
+}
+else
+{
+    var c=40;
+}
+console.log(a, b,c ); */
