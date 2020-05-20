bfe4b6b431d553d9e965c4cb6e4268ec9a367775
parikshit singh <pk223933@gmail.com>
2020-05-20 21:26:06 +0530

    learnt for of

---
+++ Front End/javascript/mini-projects using javascript/extras/for of.js
@@ -0,0 +1,16 @@
+arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
+for(let i of arr)
+{
+    console.log(i);
+}
+console.log("..................................");
+for(let i=0; i<arr.length; i++)
+{
+    console.log(arr[i]);
+}
+console.log("..................................");
+for(let i in arr)
+{
+    console.log(arr[i]);
+}
+/* 3 ways to iterate */
