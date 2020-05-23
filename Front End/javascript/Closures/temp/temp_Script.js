ef09d9f08736bef783e84b72e7761170d18fdf3a
parikshit singh <pk223933@gmail.com>
2020-05-23 19:27:43 +0530

    studied arrow functions

--- Front End/javascript/Closures/temp/temp_Script.js
+++ Front End/javascript/Closures/temp/temp_Script.js
@@ -4,7 +4,7 @@
  console.log(a); */
 
 
-var add = (function ()
+/* var add = (function ()
 {
     var counter = 0;
     return function () 
@@ -13,4 +13,29 @@ var add = (function ()
 })();
 console.log(add());
 console.log(add());
-console.log(add());
+console.log(add()); */
+
+
+
+/* arrow functions */
+/* var multiply=(x, y)=>
+{
+    return x*y;
+}
+console.log(multiply(2, 3));
+
+
+ */
+var double=x=>2*x;
+console.log(double(5));
+
+/* "use strict";
+function person(name)
+{
+  this.name=name;
+  console.log(this);
+  
+  setTimeout(()=>console.log(this), 1000)
+}
+var obj=new person("parikshit") */
+
