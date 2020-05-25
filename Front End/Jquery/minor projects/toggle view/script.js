c856044ece89ada3ebfcd71cc0bc767d0ca5fb94
parikshit singh <pk223933@gmail.com>
2020-05-25 19:00:45 +0530

    implemented

--- Front End/Jquery/minor projects/toggle view/script.js
+++ Front End/Jquery/minor projects/toggle view/script.js
@@ -1,2 +1,26 @@
 ///<reference path="typings/globals/jquery/index.d.ts" />
 
+var toggler=$('#toggler');
+var switcher=$('#switch');
+var body=$('body');
+var text=$('#text')
+toggler.click(
+    function()
+    {
+        switcher.toggleClass('right');
+        if(switcher.hasClass('right'))
+        {
+            text.css("color", "white");
+            body.css('backgroundColor', "black");
+            switcher.css('backgroundColor', 'white');
+            toggler.css('border', '2px solid white');
+        }
+        else
+        {
+            text.css("color", "black");
+            body.css('backgroundColor', "white");
+            switcher.css('backgroundColor', 'black');
+            toggler.css('border', '2px solid black');
+        }
+    }
+)
