8c0aa0af94e0613cf9543164116ab9486d14dcf0
parikshit singh <pk223933@gmail.com>
2020-05-25 07:02:46 +0530

    learnt jquery

--- Front End/Jquery/script1.js
+++ Front End/Jquery/script1.js
@@ -1,1 +1,30 @@
-<reference path="typings/globals/jquery/index.d.ts" />
+/// <reference path="typings/globals/jquery/index.d.ts" />
+//tsd install jquery --save
+$('#para1').css(
+    {
+        fontSize: "30px",
+        color: "red",
+    }
+);
+$('div').css(
+    {
+        "background-color": "rgba(50, 50, 50, 1)",
+        "width": "50px",
+        "height": "50px",
+    }
+)
+/* $('div').click(
+    function ()
+    {
+        window.alert('div clicked');
+    }
+) */
+$('div').on('click', function()
+{
+    window.alert('div clicked');
+})
+
+$('input').click(function()
+{
+    $('input').val('hello buddy');
+})
