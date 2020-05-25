e4033de4a5d53a0ff97a91af1d82fe5a03940ef8
parikshit singh <pk223933@gmail.com>
2020-05-25 21:54:02 +0530

    implemented

--- Front End/Jquery/minor projects/put balls in basket/script.js
+++ Front End/Jquery/minor projects/put balls in basket/script.js
@@ -1,2 +1,22 @@
 /// <reference path="typings/globals/jquery/index.d.ts" />
 
+var colors = ['red', 'blue', 'yellow', 'lightgrey', 'darkorchid', 'black',
+    'orange', 'deeppink', 'green', 'purple', 'saddlebrown', 'lightseagreen',
+    'deepskyblue', 'firebrick', 'crimson'];
+
+
+function add_ball()
+{
+    let random_color=colors[Math.floor(Math.random()*colors.length)];
+    let container=$('#container');
+    let ball=$('<div class="ball"></div>');
+    ball.css('backgroundColor', random_color);
+    container.append(ball);
+}
+$('button').click(
+    function(event)
+    {
+        event.preventDefault();
+        add_ball();
+    }
+)
