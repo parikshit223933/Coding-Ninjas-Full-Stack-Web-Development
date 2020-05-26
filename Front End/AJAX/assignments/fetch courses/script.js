42c178f3ca277eb6117b7d4a66e100695112ee83
parikshit singh <pk223933@gmail.com>
2020-05-26 10:32:15 +0530

    implemented

--- Front End/AJAX/assignments/fetch courses/script.js
+++ Front End/AJAX/assignments/fetch courses/script.js
@@ -1,1 +1,31 @@
 /// <reference path="typings/globals/jquery/index.d.ts" />
+/* https://codingninjas.in/api/v3/courses */
+
+
+var first_screen = $('.top-30');
+var second_screen = $('.container-fluid')
+var button = $('button');
+button.click(function ()
+{
+    first_screen.addClass('d-none');
+    second_screen.removeClass('d-none');
+
+    $.get("https://codingninjas.in/api/v3/courses",
+        function (data)
+        {
+            let response = data;
+            let all_courses = data.data.courses;
+            for (let course of all_courses)
+            {
+                console.log(course);
+                let str='<div class="col mb-4"><div class="card"><img src="'+course.preview_image_url+'" class="card-img-top" alt=""><div class="card-body"><h5 class="card-title">'+course.name+'</h5><p class="card-text">'+course.level+'</p></div></div></div>'
+                second_screen.append(str);
+            }
+        },
+    ).fail(function()
+    {
+        console.log('failed');
+        window.alert('please check your internet connection');
+        return;
+    })
+});
