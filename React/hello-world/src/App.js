71f798931da2b1ccf788c1a6cca6d986d4fcb6b5
Parikshit Singh <pk223933@gmail.com>
2020-06-25 04:53:33 +0530

    user login status implemented (basic)

--- React/hello-world/src/App.js
+++ React/hello-world/src/App.js
@@ -3,24 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const name = 'Parikshit';
+	const isLoggedIn = true;
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				{!isLoggedIn && <p>Hello World</p>}
+				{isLoggedIn && <p>Hello {name}</p>}
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- React/hello-world/src/index.js
+++ React/hello-world/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App';/* the thing which i am importing, its name should be in capitals which will help react to distinguish a particular HTML element from the react DOM component and we will use camelcase for that */
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
